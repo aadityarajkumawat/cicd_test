adf98aa65415863db25d259b26f58053c6e9bd9b
Aditya Raj Kumawat <arkumawat78@gmail.com>
2025-04-09 14:52:37 -0400

    appspec

--- start_app.sh
+++ start_app.sh
@@ -6,4 +6,4 @@ HOST="0.0.0.0"
 PORT=8000
 
 echo "Starting FastAPI server at http://$HOST:$PORT"
-uvicorn $APP_MODULE --host $HOST --port $PORT
+python -m uvicorn $APP_MODULE --host $HOST --port $PORT
