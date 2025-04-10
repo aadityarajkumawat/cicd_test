a8f26bfb97cd154bc9dd488da4008d4ff486811e
Aditya Raj Kumawat <arkumawat78@gmail.com>
2025-04-09 21:59:04 -0400

    here

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World from CICD here"}
+    return {"message": "Hello World from CICD"}
 
 
 if __name__ == "__main__":
