f0caa1309f812083b38ff739f6ecfdb76359f0c1
Aditya Raj Kumawat <arkumawat78@gmail.com>
2025-04-12 15:44:22 -0400

    route updated

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World from CICD here, update1!"}
+    return {"message": "Hello World from CICD here, updat!"}
 
 
 if __name__ == "__main__":
