78738a664c30acf5dce64197ef73dd72029f0ec1
Aditya Raj Kumawat <arkumawat78@gmail.com>
2025-04-12 16:06:02 -0400

    updated stop app

--- scripts/stop_app1.sh
+++ scripts/stop_app1.sh
@@ -2,9 +2,23 @@
 
 set -e
 
-sudo su - root
+# No need to switch to root if the script is already run with sudo
+# sudo su - root
 
-echo "Running docker stop"
-docker stop $(docker ps -a -q)
-echo "Running docker rm"
-docker rm $(docker ps -a -q)
+echo "Checking for running containers to stop..."
+containers=$(docker ps -q)
+if [ -n "$containers" ]; then
+    echo "Running docker stop"
+    docker stop $containers
+else
+    echo "No running containers to stop"
+fi
+
+echo "Checking for all containers to remove..."
+all_containers=$(docker ps -a -q)
+if [ -n "$all_containers" ]; then
+    echo "Running docker rm"
+    docker rm $all_containers
+else
+    echo "No containers to remove"
+fi
